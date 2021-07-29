765c5cb4a13899cc0450112c54e76f63ee908b11
Francis Agulto <francis.agulto@wpengine.com>
2021-07-29 11:03:41 -0500

    initial check

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Article from "./components/Article";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState("webdev");
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/" + subreddit + ".json?limit=9").then(
+      (res) => {
+        if (res.status != 200) {
+          console.log("error");
+          return;
+        }
+
+        res.json().then((data) => {
+          if (data != null) {
+            setArticles(data.data.children);
+          }
+        });
+      }
+    );
+  }, [subreddit]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <input
+          class="subreddit_input"
+          onChange={(e) => setSubreddit(e.target.value)}
+          value={subreddit}
+        />
       </header>
+      <div className="articles">
+        {articles != null
+          ? articles.map((article, index) => (
+              <Article key={index} article={article.data} />
+            ))
+          : ""}
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <input class="subreddit_input" value="WebDev" />
+    </header>
+  );
+}
+
+export default Header;
